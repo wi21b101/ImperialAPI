ab343e5902ccad0ecc54cf7042b31e797ef12599
Leona Widhalm <wi21b101@technikum-wien.at>
2022-06-22 15:23:08 +0200

    Initial commit

---
+++ src/main/java/com/example/imperialapi/ImperialApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.imperialapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImperialApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImperialApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/imperialapi/ImperialApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.imperialapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImperialApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
